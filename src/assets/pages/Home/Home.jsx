87c9afe99ac4baf8efcf963235db3a1921877f51
Vlad Beltran <puentes.studio@gmail.com>
2024-03-16 14:07:57 +0100

    nav done, working on home

---
+++ src/assets/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Home.css";
+import NavBar from "../../../components/navBar/NavBar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import UnikaLogo from "../../assets/extra/unika-logos/logo-unika-hr.png";
+import "./NavBar.css";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="nav-bar">
+        <div className="nav-logo">
+          <img src={UnikaLogo} className="UnikaHR-logo" alt="UnikaHR logo" />
+        </div>
+        <div className={`nav-items ${isOpen && "open"}`}>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/about">ABOUT</NavLink>
+          <NavLink to="/services">SERVICES</NavLink>
+          <NavLink to="/people">PEOPLE</NavLink>
+          <NavLink to="/contact">CONTACT</NavLink>
+        </div>
+        <div
+          className={`nav-toggle ${isOpen && "open"}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
