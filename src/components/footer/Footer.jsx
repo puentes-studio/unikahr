ef27de3b6c6bc1b123def637b70882f4acffff9a
Vlad Beltran <puentes.studio@gmail.com>
2024-03-16 15:28:45 +0100

    home structure and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css";
 import UnikaLogo from "../../assets/extra/unika-logos/logo-unika-hr.png";
 import Linkedin from "../../assets/extra/linkedin-app-white-icon.webp";
-// import Github from "../../assets/extra";
+import Instagram from "../../assets/extra/Instagram-logo-white.png";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
@@ -14,24 +14,39 @@ const Footer = () => {
           </div>
           <h4 className="footer-title">Vlad Beltran Puentes</h4>
           <div className="social-icons">
-            <a href="" target="_blank">
+            <a
+              href="https://www.linkedin.com/company/unikahr/about/"
+              target="_blank"
+            >
               <img className="linkedin-size" src={Linkedin} alt="LinkedIn" />
             </a>
-            {/* <a href="https://github.com/" target="_blank">
-              <img className="github-size" src={Github} alt="GitHub" />
-            </a> */}
+            <a href="https://www.instagram.com/unika_hr/" target="_blank">
+              <img className="instagram-size" src={Instagram} alt="instagram" />
+            </a>
           </div>
         </div>
 
         <ul className="footer-list">
           <li>
-            <NavLink to="/about">About Me</NavLink>
+            <NavLink to="/about">About</NavLink>
           </li>
           <li>
-            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/projects">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">People</NavLink>
           </li>
           <li>
             <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Privacy Policy</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Cookies Policy</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Terms & Conditions</NavLink>
           </li>
         </ul>
       </div>
