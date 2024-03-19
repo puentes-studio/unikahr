8a509af6762c524ea453a896cbf711231d7b327a
Vlad Beltran <puentes.studio@gmail.com>
2024-03-19 18:14:32 +0100

    people page working but no styles yet

--- src/assets/pages/people/People.jsx
+++ src/assets/pages/people/People.jsx
@@ -1,6 +1,5 @@
-import badgeHuman from "../../extra/png/human_hr_.png";
-import unikaFrame from "../../extra/png/frame_2.png";
 import NavBar from "../../../components/navBar/NavBar";
+import peopleData from "./PeopleData";
 import "./People.css";
 import Footer from "../../../components/footer/Footer";
 
@@ -9,67 +8,27 @@ const People = () => {
     <div>
       <NavBar />
       <main>
-        <div className="human-container container-home">
-          <div className="human-title-img-container">
-            <img
-              src={badgeHuman}
-              className="human-image-hr"
-              alt="Putting the human"
-            />
-            <h2 className="human-title">
-              BACK INTO <br /> HUMAN RESOURCES
-            </h2>
-          </div>
-
-          <p className="human-description">
-            We specialise in providing HR support for small and medium
-            businesses.
-          </p>
-        </div>
-
-        {/* // Division # 1 */}
-
-        <div className="unika-frame-container container-home">
-          <div className="frame-title-img-container">
-            <h2 className="frame-title">
-              SIMPLE
-              <br />
-              CURIOUS
-              <br />
-              BOLD
-            </h2>
-            <img
-              src={unikaFrame}
-              className="frame-image-hr"
-              alt="Putting the human"
-            />
-          </div>
-          <h4>WE ARE UNIKA HR, THE PEOPLE BUSINESS.</h4>
-          <p className="frame-description">
-            A team of HR professionals who share the same values and beliefs
-            about people at work. We’ve recognised that Human Resources can
-            become very disassociated from…. humans and we want to put that
-            right!
-          </p>
-        </div>
-
-        {/* // Division # 2 */}
-
-        <div className="strategy-container">
-          <div className="strategy-title-img-container">
-            {/* <h2 className="strategy-title">STRATEGY</h2> */}
-            {/* <img
-              src={hrStrategy}
-              className="strategy-image-hr"
-              alt="Putting the human"
-            /> */}
-          </div>
-
-          <p className="strategy-description">
-            WE KNOW OUR STUFF AND WANT TO SHARE THIS KNOWLEDGE IN A WAY THAT YOU
-            AND YOUR PEOPLE CAN EASILY UNDERSTAND AND FIND USEFUL.
-          </p>
-        </div>
+        <ul className="container">
+          <li className="container-people">
+            {peopleData.map((people) => (
+              <div key={people.id} className="people-title-img-container">
+                <img
+                  src={people.imageUrl}
+                  className="people-image-hr"
+                  alt="Putting the human"
+                />
+                <div className="info-container">
+                  <h2 className="people-name">{people.name}</h2>
+                  <h5 className="people-title">{people.position}</h5>
+                  <div
+                    className="people-description"
+                    dangerouslySetInnerHTML={{ __html: people.description }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </li>
+        </ul>
       </main>
       <Footer />
     </div>
---
+++ src/assets/pages/people/PeopleData.js
@@ -0,0 +1,29 @@
+import frameDominika from "../../extra/png/frame_1.png";
+// import UnikaLego from "../../assets/extra/lego/LEGO_hr.png";
+
+const peopleData = [
+  {
+    id: "1",
+    imageUrl: frameDominika,
+    name: "Dominika",
+    position: `Chief ‘Human’ Officer`,
+    description: `<p>It’s been 16 years since I entered the world of work and to date there are two employers who stood out to me.</p>\
+      <p>glh Hotels, here I learnt the HR ropes and the importance of team work. The creativity of people around me brought a lot of fun and whilst work was endless, together we made things happen. I’ve kept that.</p>\
+      <p>Harbour & Jones, we were PEOPLE there. People whose experiences and opinions as individuals were listened to. New ideas were welcomed and embraced and although we couldn’t always do everything in our HR dreams there was a sense of appreciation for what we did. It was great !</p>\
+      <p>Outside of work, I am a bit of a sport ‘addict’ and one who values friendships and family.</p>\
+      <p>And here I am now, offering my help to embrace that PEOPLE touch in your business !</p>`,
+  },
+  {
+    id: "2",
+    imageUrl: frameDominika,
+    name: "Dominika",
+    position: `Chief ‘Human’ Officer`,
+    description: `<p>It’s been 16 years since I entered the world of work and to date there are two employers who stood out to me.</p>\
+      <p>glh Hotels, here I learnt the HR ropes and the importance of team work. The creativity of people around me brought a lot of fun and whilst work was endless, together we made things happen. I’ve kept that.</p>\
+      <p>Harbour & Jones, we were PEOPLE there. People whose experiences and opinions as individuals were listened to. New ideas were welcomed and embraced and although we couldn’t always do everything in our HR dreams there was a sense of appreciation for what we did. It was great !</p>\
+      <p>Outside of work, I am a bit of a sport ‘addict’ and one who values friendships and family.</p>\
+      <p>And here I am now, offering my help to embrace that PEOPLE touch in your business !</p>`,
+  },
+];
+
+export default peopleData;
