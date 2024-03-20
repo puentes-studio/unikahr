e981fd1b87bc3db0b303bca0084c4c3d89fde839
Vlad Beltran <puentes.studio@gmail.com>
2024-03-20 13:16:03 +0100

    privacy policy

--- src/assets/pages/cookiesPolicy/CookiesPolicy.jsx
+++ src/assets/pages/cookiesPolicy/CookiesPolicy.jsx
@@ -1,5 +1,3 @@
-import badgeHuman from "../../extra/png/human_hr_.png";
-import unikaFrame from "../../extra/png/frame_2.png";
 import NavBar from "../../../components/navBar/NavBar";
 import "./CookiesPolicy.css";
 import Footer from "../../../components/footer/Footer";
@@ -9,65 +7,123 @@ const CookiesPolicy = () => {
     <div>
       <NavBar />
       <main>
-        <div className="human-container container-home">
-          <div className="human-title-img-container">
-            <img
-              src={badgeHuman}
-              className="human-image-hr"
-              alt="Putting the human"
-            />
-            <h2 className="human-title">
-              BACK INTO <br /> HUMAN RESOURCES
-            </h2>
-          </div>
-
-          <p className="human-description">
-            We specialise in providing HR support for small and medium
-            businesses.
-          </p>
-        </div>
-
-        {/* // Division # 1 */}
-
-        <div className="unika-frame-container container-home">
-          <div className="frame-title-img-container">
-            <h2 className="frame-title">
-              SIMPLE
-              <br />
-              CURIOUS
-              <br />
-              BOLD
-            </h2>
-            <img
-              src={unikaFrame}
-              className="frame-image-hr"
-              alt="Putting the human"
-            />
-          </div>
-          <h4>WE ARE UNIKA HR, THE PEOPLE BUSINESS.</h4>
-          <p className="frame-description">
-            A team of HR professionals who share the same values and beliefs
-            about people at work. We’ve recognised that Human Resources can
-            become very disassociated from…. humans and we want to put that
-            right!
-          </p>
-        </div>
-
-        {/* // Division # 2 */}
-
-        <div className="strategy-container">
-          <div className="strategy-title-img-container">
-            {/* <h2 className="strategy-title">STRATEGY</h2> */}
-            {/* <img
-              src={hrStrategy}
-              className="strategy-image-hr"
-              alt="Putting the human"
-            /> */}
-          </div>
-
-          <p className="strategy-description">
-            WE KNOW OUR STUFF AND WANT TO SHARE THIS KNOWLEDGE IN A WAY THAT YOU
-            AND YOUR PEOPLE CAN EASILY UNDERSTAND AND FIND USEFUL.
+        <div className="container-conditions">
+          <p className="conditions-description">
+            Cookies Policy
+            <br />
+            Last updated: March 20, 2024
+            <br />
+            This Cookies Policy explains what Cookies are and how We use them.
+            You should read this policy so You can understand what type of
+            cookies We use, or the information We collect using Cookies and how
+            that information is used. This Cookies Policy has been created with
+            the help of the Cookies Policy Generator.
+            <br />
+            Cookies do not typically contain any information that personally
+            identifies a user, but personal information that we store about You
+            may be linked to the information stored in and obtained from
+            Cookies. For further information on how We use, store and keep your
+            personal data secure, see our Privacy Policy.
+            <br />
+            We do not store sensitive personal information, such as mailing
+            addresses, account passwords, etc. in the Cookies We use.
+            <br />
+            Interpretation and Definitions
+            <br />
+            Interpretation
+            <br />
+            The words of which the initial letter is capitalized have meanings
+            defined under the following conditions. The following definitions
+            shall have the same meaning regardless of whether they appear in
+            singular or in plural.
+            <br />
+            Definitions
+            <br />
+            For the purposes of this Cookies Policy:
+            <br />
+            Company (referred to as either “the Company”, “We”, “Us” or “Our” in
+            this Cookies Policy) refers to UnikaHR.
+            <br />
+            Cookies means small files that are placed on Your computer, mobile
+            device or any other device by a website, containing details of your
+            browsing history on that website among its many uses.
+            <br />
+            Website refers to UnikaHR, accessible from https://unikahr.com
+            <br />
+            You means the individual accessing or using the Website, or a
+            company, or any legal entity on behalf of which such individual is
+            accessing or using the Website, as applicable.
+            <br />
+            The use of the Cookies
+            <br />
+            Type of Cookies We Use
+            <br />
+            Cookies can be “Persistent” or “Session” Cookies. Persistent Cookies
+            remain on your personal computer or mobile device when You go
+            offline, while Session Cookies are deleted as soon as You close your
+            web browser.
+            <br />
+            We use both session and persistent Cookies for the purposes set out
+            below:
+            <br />
+            Necessary / Essential CookiesType: Session CookiesAdministered by:
+            UsPurpose: These Cookies are essential to provide You with services
+            available through the Website and to enable You to use some of its
+            features. They help to authenticate users and prevent fraudulent use
+            of user accounts. Without these Cookies, the services that You have
+            asked for cannot be provided, and We only use these Cookies to
+            provide You with those services.
+            <br />
+            Functionality CookiesType: Persistent CookiesAdministered by:
+            UsPurpose: These Cookies allow us to remember choices You make when
+            You use the Website, such as remembering your login details or
+            language preference. The purpose of these Cookies is to provide You
+            with a more personal experience and to avoid You having to re-enter
+            your preferences every time You use the Website.
+            <br />
+            Your Choices Regarding Cookies
+            <br />
+            If You prefer to avoid the use of Cookies on the Website, first You
+            must disable the use of Cookies in your browser and then delete the
+            Cookies saved in your browser associated with this website. You may
+            use this option for preventing the use of Cookies at any time.
+            <br />
+            If You do not accept Our Cookies, You may experience some
+            inconvenience in your use of the Website and some features may not
+            function properly.
+            <br />
+            If You’d like to delete Cookies or instruct your web browser to
+            delete or refuse Cookies, please visit the help pages of your web
+            browser.
+            <br />
+            For the Chrome web browser, please visit this page from Google:
+            https://support.google.com/accounts/answer/32050
+            <br />
+            For the Internet Explorer web browser, please visit this page from
+            Microsoft: http://support.microsoft.com/kb/278835
+            <br />
+            For the Firefox web browser, please visit this page from Mozilla:
+            https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored
+            <br />
+            For the Safari web browser, please visit this page from Apple:
+            https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac
+            <br />
+            For any other web browser, please visit your web browser’s official
+            web pages.
+            <br />
+            More Information about Cookies
+            <br />
+            You can learn more about cookies: What Are Cookies?.
+            <br />
+            Contact Us
+            <br />
+            If you have any questions about this Cookies Policy, You can contact
+            us:
+            <br />
+            By email: info@unikahr.com
+            <br />
+            By visiting this page on our website: https://unikahr.com/contact/
+            <br />
           </p>
         </div>
       </main>
